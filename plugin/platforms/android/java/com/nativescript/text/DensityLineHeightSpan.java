ef0d20c93d6059401f1b24468f1a4c16cb63c0aa
Martin Guillon <martin.guillon@akylas.fr>
2022-01-11 10:25:01 +0100

    fix(android): span lineHeight fix

---
+++ plugin/platforms/android/java/com/nativescript/text/DensityLineHeightSpan.java
@@ -0,0 +1,43 @@
+package com.nativescript.text;
+
+import android.annotation.SuppressLint;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.TextPaint;
+import android.text.style.LineHeightSpan;
+import java.lang.CharSequence;
+
+@SuppressLint("ParcelCreator")
+public class DensityLineHeightSpan implements LineHeightSpan.WithDensity {
+    private int mSize;
+    private static float sProportion = 0;
+
+    public HeightSpan(int size) {
+        mSize = size;
+    }
+
+    public void chooseHeight(CharSequence text, int start, int end,
+                             int spanstartv, int v,
+                             Paint.FontMetricsInt fm) {
+        // Should not get called, at least not by StaticLayout.
+        chooseHeight(text, start, end, spanstartv, v, fm, null);
+    }
+
+    public void chooseHeight(CharSequence text, int start, int end,
+                             int spanstartv, int v,
+                             Paint.FontMetricsInt fm, TextPaint paint) {
+        int size = mSize;
+        if (paint != null) {
+            size *= paint.density;
+        }
+
+        final int originHeight = fm.descent - fm.ascent;
+        // If original height is not positive, do nothing.
+        if (originHeight <= 0) {
+            return;
+        }
+        final float ratio = size * 1.0f / originHeight;
+        fm.descent = Math.round(fm.descent * ratio);
+        fm.ascent = fm.descent - size;
+    }
+}
--- plugin/platforms/android/java/com/nativescript/text/Font.java
+++ plugin/platforms/android/java/com/nativescript/text/Font.java
@@ -377,7 +377,9 @@ public class Font {
         }
         Double lineHeight =  span.optDouble("lineHeight");
         if (!Double.isNaN(lineHeight)) {
-            ssb.setSpan(new HeightSpan(lineHeight.intValue()), start, end,
+            // we use standard for now though it does not take density into account
+            // is this what we want?
+            ssb.setSpan(new android.text.style.LineHeightSpan.Standard(lineHeight.intValue()), start, end,
                     android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
 
