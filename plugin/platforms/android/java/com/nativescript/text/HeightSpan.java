a8719673ca84530724cd370db07edf1b21924d59
Martin Guillon <martin.guillon@akylas.fr>
2022-02-14 15:06:29 +0100

    fix(android): fix missing class for createSpannable from JS

---
+++ plugin/platforms/android/java/com/nativescript/text/HeightSpan.java
@@ -0,0 +1,10 @@
+
+package com.nativescript.text;
+
+import android.text.style.LineHeightSpan;
+
+public class HeightSpan extends LineHeightSpan.Standard {
+    public HeightSpan(int size) {
+        super(size);
+    }
+}
