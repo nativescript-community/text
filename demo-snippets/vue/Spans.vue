ee3f63ead15f6a41e20e77eb54c6ab4e42d8aafa
farfromrefuge <dev@akylas.fr>
2024-01-18 22:27:37 +0100

    chore: demo comparing all text rendering in N

--- demo-snippets/vue/Spans.vue
+++ demo-snippets/vue/Spans.vue
@@ -2,16 +2,21 @@
     <Page>
         <ActionBar title="Simple Demo" />
         <StackLayout>
-           <Button
-        verticalTextAlignment="center"
-        horizontalTextAlignment="center"
-        textAlignment="center"
-    >
-    <FormattedString>
-        <Span fontWeight="bold" text="A" verticalAlignment="center" fontSize="35" />
-        <Span text="test" fontSize="24" verticalAlignment="center" />
-    </FormattedString>
-    </Button>
+            <Label text="test" fontSize="16" />
+            <HTMLLabel text="test" fontSize="16" />
+            <HTMLLabel :text="nativeText" fontSize="16" />
+            <HTMLLabel html='<span style="color:red;">test</span>' fontSize="16" />
+            <CanvasLabel height="22">
+                <CSpan fontSize="16" text="test" />
+            </CanvasLabel>
+            <CanvasView height="20" @draw="onDraw" />
+            <CanvasView height="40" @draw="onDraw1" />
+            <Button verticalTextAlignment="center" horizontalTextAlignment="center" textAlignment="center">
+                <FormattedString>
+                    <Span fontWeight="bold" text="A" verticalAlignment="center" fontSize="35" />
+                    <Span text="test" fontSize="24" verticalAlignment="center" />
+                </FormattedString>
+            </Button>
         </StackLayout>
     </Page>
 </template>
@@ -20,11 +25,32 @@
 import Vue from 'nativescript-vue';
 import { Component } from 'vue-property-decorator';
 import { Frame } from '@nativescript/core';
-
+import { createNativeAttributedString } from '@nativescript-community/text';
+import { Canvas, CanvasView, LayoutAlignment, Paint, StaticLayout } from '@nativescript-community/ui-canvas';
+const nativeText = createNativeAttributedString({
+    spans: [
+        {
+            fontSize: 16,
+            text: 'test'
+        }
+    ]
+});
+const textPaint = new Paint();
+textPaint.setTextSize(16);
 @Component
 export default class Simple extends Vue {
+    nativeText = nativeText;
     onBack() {
         Frame.topmost().goBack();
+    }
+    onDraw({ canvas, object }: { canvas: Canvas; object: CanvasView }) {
+        canvas.drawText('test', 0, 16, textPaint);
+    }
+    onDraw1({ canvas, object }: { canvas: Canvas; object: CanvasView }) {
+        const w = canvas.getWidth();
+        const staticLayout = new StaticLayout(nativeText, textPaint, w, LayoutAlignment.ALIGN_NORMAL, 1, 0, true, 'end');
+        canvas.translate(0, 0);
+        staticLayout.draw(canvas);
     }
 }
 </script>
--- demo-snippets/vue/install.ts
+++ demo-snippets/vue/install.ts
@@ -4,6 +4,9 @@ import { overrideSpanAndFormattedString } from '@nativescript-community/text';
 
 export function installPlugin() {
     Vue.registerElement('HTMLLabel', () => require('@nativescript-community/ui-label').Label);
+    Vue.registerElement('CanvasView', () => require('@nativescript-community/ui-canvas').CanvasView);
+    Vue.registerElement('CanvasLabel', () => require('@nativescript-community/ui-canvaslabel').CanvasLabel);
+    Vue.registerElement('CSPan', () => require('@nativescript-community/ui-canvaslabel').Span);
     overrideSpanAndFormattedString();
 }
 
